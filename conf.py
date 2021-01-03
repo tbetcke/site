e7e3b139b265fd4c62af6b7edf8e16a6db1d5819
Timo Betcke <timo.betcke@gmail.com>
2021-01-03 16:12:33 +0000

    First post and Github action.

--- conf.py
+++ conf.py
@@ -142,7 +142,8 @@ NAVIGATION_LINKS = {
     DEFAULT_LANG: (
         ("/archive.html", "Archive"),
         ("/categories/", "Tags"),
-        ("/rss.xml", "RSS feed"),
+#        ("/rss.xml", "RSS feed"),
+        ("/pages/about/", "About Me"),
     ),
 }
 
@@ -267,6 +268,7 @@ TIMEZONE = "Europe/London"
 # Used by babel.dates, CLDR style: http://cldr.unicode.org/translation/date-time
 # You can also use 'full', 'long', 'medium', or 'short'
 # DATE_FORMAT = 'yyyy-MM-dd HH:mm'
+DATE_FORMAT = 'yyyy-MM-dd'
 
 # Date format used to display post dates, if local dates are used. (translatable)
 # Used by Luxon: https://moment.github.io/luxon/docs/manual/formatting
---
+++ pages/about.rst
@@ -0,0 +1,12 @@
+.. title: About me
+.. slug: about
+.. date: 2021-01-03 15:32:25 UTC
+.. tags: 
+.. category: 
+.. link: 
+.. description: 
+.. type: text
+
+I am a Computational Mathematician working in the `Department of Mathematics <https://ucl.ac.uk/maths>`_ at `University College London <https://ucl.ac.uk>`_.
+
+For an up-to-date list of my publications see my `Google Scholar Site <https://scholar.google.com/citations?hl=en&user=HCAkiTYAAAAJ&view_op=list_works&sortby=pubdate>`_.
